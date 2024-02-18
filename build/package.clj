abcff3d0adc9076ed1d2ee114de7db72cea2bb51
ajchemist <1694505+aJchemist@users.noreply.github.com>
2024-02-18 15:57:59 +0900

    Fixed pom.xml licenses

--- build/package.clj
+++ build/package.clj
@@ -32,10 +32,10 @@
                     :scm       {:url (or scm-url github-scm-url "")}
                     :src-dirs  ["src/core"]
                     :pom-data  [[:licenses
-                                [:license
-                                 [:name "MIT License"]
-                                 [:url "https://opensource.org/license/mit/"]
-                                 [:distribution "repo"]]]]})
+                                 [:license
+                                  [:name "MIT License"]
+                                  [:url "https://opensource.org/license/mit/"]
+                                  [:distribution "repo"]]]]})
   (build/copy-file {:src    (build/pom-path {:class-dir class-dir :lib lib})
                     :target "pom.xml"})
   (build/copy-dir {:src-dirs   ["src/core"]
